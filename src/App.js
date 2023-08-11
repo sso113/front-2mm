8cc3a96e512d07f7ef93fbb5d91cad24888960e4
소영 <marbleksy@gmail.com>
2023-08-11 16:21:26 +0900

    프론트 연동

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import Login from "./pages/Login";
 import Passwd from "./pages/Passwd";
 //import Splash from "./pages/Splash";
 import Home from "./pages/Home";
+import Signup1_old from "./pages/Signup1_old";
+import Signup2_old from "./pages/Signup2_old";
+import Signup3_old from "./pages/Signup3_old";
+import Signup1_new from "./pages/Signup1_new";
+import Signup2_new from "./pages/Signup2_new";
+import Signup3_new from "./pages/Signup3_new";
+import Signup4_new from "./pages/Signup4_new";
+import Signup5_new from "./pages/Signup5_new";
+import Date_List from "./pages/Date_List";
+import Date_Detail from "./pages/Date_Detail";
+import Date_Plus from "./pages/Date_Plus";
+import Date_Write from "./pages/Date_Write";
 
 function App() {
   return (
@@ -15,6 +27,18 @@ function App() {
           <Route path="/Passwd" element={<Passwd />} />{" "}
           <Route path="/Home" element={<Home />} />{" "}
           <Route path="/Passwd" element={<Passwd />} />{" "}
+          <Route path="/Signup1_old" element={<Signup1_old />} />{" "}
+          <Route path="/Signup2_old" element={<Signup2_old />} />{" "}
+          <Route path="/Signup3_old" element={<Signup3_old />} />{" "}
+          <Route path="/Signup1_new" element={<Signup1_new />} />{" "}
+          <Route path="/Signup2_new" element={<Signup2_new />} />{" "}
+          <Route path="/Signup3_new" element={<Signup3_new />} />{" "}
+          <Route path="/Signup4_new" element={<Signup4_new />} />{" "}
+          <Route path="/Signup5_new" element={<Signup5_new />} />{" "}
+          <Route path="/Date_List" element={<Date_List />} />{" "}
+          <Route path="/Date_Detail" element={<Date_Detail />} />{" "}
+          <Route path="/Date_Plus" element={<Date_Plus />} />{" "}
+          <Route path="/Date_Write" element={<Date_Write />} />{" "}
         </Routes>
       </div>
     </BrowserRouter>
